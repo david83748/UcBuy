0460a61426e82a861a0ae65129056a52213ebe11
david83748 <david.vashchenko@gmail.com>
2021-12-21 12:28:59 +0200

    add new style for img

--- js/calculator.js
+++ js/calculator.js
@@ -4,52 +4,30 @@
 
     document.getElementById('resultt').innerHTML = text;
     if (amountItem() < 60) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0380.PNG" alt="special offers 60">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0380.PNG" alt="special offers 60">')
     } else if (amountItem() <= 180) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0381.PNG" alt="special offers 180">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0381.PNG" alt="special offers 180">')
     } else if (amountItem() <= 300) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0384.PNG" alt="special offers 300">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0384.PNG" alt="special offers 300">')
 
     } else if (amountItem() <= 600) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0385.PNG" alt="special offers 600">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0385.PNG" alt="special offers 600">')
 
     } else if (amountItem() <= 900) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0385.PNG" alt="special offers 900">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0386.PNG" alt="special offers 900">')
 
     } else if (amountItem() <= 1500) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0387.PNG" alt="special offers 1500">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0387.PNG" alt="special offers 1500">')
 
     } else if (amountItem() <= 3000) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0388.PNG" alt="special offers 3000">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items"  src="./imgs/IMG_0388.PNG" alt="special offers 3000">')
 
     } else if (amountItem() <= 6000) {
-      document.getElementById('offer').innerHTML = ('<img src="./imgs/IMG_0390.PNG" alt="special offers 6000">')
+      document.getElementById('offer').innerHTML = ('<img class="calculator__img-items" src="./imgs/IMG_0390.PNG" alt="special offers 6000">')
 
     } 
-
-      
-
-      
+    
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
   
   function calculateUc() {
     
@@ -90,6 +68,10 @@ function textFromUsdToUah(rovno) {
 
   return `${symaItem()} $ это ${rovno} ГРН`
 };
+
+// function textFromUsdToUah(syma, rovno) {
+//   return `${syma} $ это ${rovno} ГРН`
+// };
 
 function symaItem() {
 
